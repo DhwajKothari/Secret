bc478a78cd083ca00e02c7a8a6729d3af053e262
DhwajKothari <dhwaj.kothari@yahoo.in>
2020-06-02 23:47:52 +0530

    With Hashing improved

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const userSchema= new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=new mongoose.model("user",userSchema)
 
