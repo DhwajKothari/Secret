c4ab261617b25aec1e270f9e76168454e5aede25
DhwajKothari <dhwaj.kothari@yahoo.in>
2021-05-22 23:58:24 +0530

    passport for authentication

--- app.js
+++ app.js
@@ -1,93 +1,105 @@
 //jshint esversion:6
-require('dotenv').config();
-const express=require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
-const app=express();
-const mongoose=require("mongoose");
-const session=require("express-session");
-const passport=require("passport");
-const passportLocalMongoose=require("passport-local-mongoose");
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 app.use(express.static("public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology: true});
-mongoose.set("useCreateIndex",true);
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
 
-const userSchema= new mongoose.Schema({
-  email:String,
-  password:String
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
-const User=new mongoose.model("user",userSchema);
+
+const User = new mongoose.model("user", userSchema);
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/secrets",function(req,res){
-  if(req.isAuthenticated()){
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
     res.render("secrets");
-  }else{
+  } else {
     res.redirect("/login");
   }
 });
-app.route("/login")
-.get(function(req,res){
-  res.render("login");
-})
-.post(function(req,res){
-  const user=new User({
-    username:req.body.username,
-    password:req.body.password
-  });
-  req.login(user,function(err){
-    if(err){
-      console.log(err);
-    }else{
-      passport.authenticate("local")(req,res,function(){
-        res.redirect("/secrets");
-      });
-    }
+app
+  .route("/login")
+  .get(function (req, res) {
+    res.render("login");
   })
-});
-app.route("/register")
-.get(function(req,res){
-  res.render("register");
-})
-.post(function(req,res){
-  User.register({username:req.body.username},req.body.password,function(err,user){
-    if(err){
-      console.log(err);
-      res.redirect("/register");
-    }else{
-      passport.authenticate("local")(req,res,function(){
-        res.redirect("/secrets");
-      });
-    }
+  .post(function (req, res) {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    });
   });
-});
+app
+  .route("/register")
+  .get(function (req, res) {
+    res.render("register");
+  })
+  .post(function (req, res) {
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
+        } else {
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets");
+          });
+        }
+      }
+    );
+  });
 
-app.get('/logout', function(req, res){
+app.get("/logout", function (req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Server up and running on port 3000.");
 });
